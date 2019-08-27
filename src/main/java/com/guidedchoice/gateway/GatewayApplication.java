e659b1e781ab86b61d83a118d755f9271dbd6fc4
mbronshteyn <michael.bronshteyn@gmail.com>
2019-08-27 15:54:11 -0700

    remove security dependenchy

--- src/main/java/com/guidedchoice/gateway/GatewayApplication.java
+++ src/main/java/com/guidedchoice/gateway/GatewayApplication.java
@@ -1,43 +1,60 @@
 package com.guidedchoice.gateway;
 
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerRequest;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 @SpringBootApplication
 @RestController
-@EnableConfigurationProperties( UriConfiguration.class )
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class GatewayApplication {
 
-	@Autowired
-    DiscoveryClient discoveryClient;
-
     @Bean
+<<<<<<< Updated upstream
     public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) throws Exception {
 
     String httpUri = uriConfiguration.getHttpbin();
+=======
+    public RouteLocator myRoutes( RouteLocatorBuilder builder ) throws Exception {
+>>>>>>> Stashed changes
 
 		return builder.routes()
-                .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri(httpUri))
+//                .route(p -> p
+//                        .path("/get")
+//                        .filters(f -> f.addRequestHeader("Hello", "World"))
+//                        .uri(httpUri))
 				.route(p -> p
+<<<<<<< Updated upstream
 						.path("/sentence-client/**")
             .filters( f -> f.rewritePath(
                         		"/sentence-client/(?<segment>.*)", "/$\\{segment}")
@@ -45,10 +62,28 @@ public class GatewayApplication {
 						)
 						.uri( "lb://SENTENCE" ))
             .build();
+=======
+						.path("/sentence-service/**")
+                        .filters( f-> f
+								.rewritePath(
+                        		"/sentence-service/(?<segment>.*)", "/$\\{segment}")
+                                .retry( 3 )
+						)
+						.uri( "lb://SENTENCE:8082" )
+				)
+//                .route(p -> p
+//                        .host("*.hystrix.com")
+//                        .filters(f -> f.hystrix(config -> config
+//                                        .setName("mycmd")
+//                                        .setFallbackUri("forward:/fallback")))
+//                        .uri(httpUri))
+                .build();
+>>>>>>> Stashed changes
     }
 
-    @Autowired
-    DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator;
+//
+//    @Autowired
+//    DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator;
 
 //	@RequestMapping("/fallback")
 //	public Mono<String> fallback() {
@@ -64,20 +99,7 @@ public class GatewayApplication {
 }
 
 
-@Configuration
-@ConfigurationProperties( prefix = "uri" )
-class UriConfiguration {
 
-	private String httpbin = "http://httpbin.org:80";
-
-	public String getHttpbin() {
-		return httpbin;
-	}
-
-	public void setHttpbin(String httpbin) {
-		this.httpbin = httpbin;
-	}
-}
 
 @Configuration
 @EnableDiscoveryClient
@@ -86,10 +108,11 @@ class GatewayDiscoveryConfiguration {
 	@Autowired
 	DiscoveryLocatorProperties discoveryLocatorProperties;
 
-	@Bean
-	public DiscoveryClientRouteDefinitionLocator
-	discoveryClientRouteLocator(DiscoveryClient discoveryClient) {
-		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, discoveryLocatorProperties );
-	}
+
+//	@Bean
+//	public DiscoveryClientRouteDefinitionLocator
+//	discoveryClientRouteLocator(DiscoveryClient discoveryClient) {
+//		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, discoveryLocatorProperties );
+//	}
 }
 
